2b270dd51ea5fc7e19b9355edf7225ed87bec92e
raratiru <tantiras@yandex.com>
2019-01-26 21:55:18 +0200

    Upload to Pypi

--- thepysec/__init__.py
+++ thepysec/__init__.py
@@ -8,7 +8,7 @@
 #
 #       Creation Date : Fri 28 Dec 2018 03:36:03 PM EET
 #
-#       Last Modified : Sat 26 Jan 2019 08:09:25 PM EET
+#       Last Modified : Sat 26 Jan 2019 09:55:03 PM EET
 #
 #       Developer : raratiru  | https://twitter.com/raratiru
 #
@@ -17,6 +17,6 @@
 from thepysec.john.version import get_version
 
 
-VERSION = (1, 0, 0, 'alpha', 1)
+VERSION = (1, 0, 0, 'alpha', 2)
 
 __version__ = get_version(VERSION)
