9afc9c6066b32a2cf5e3f9616c7dd75133152617
George Tantiras <raratiru@users.noreply.github.com>
2019-10-27 13:22:19 +0200

    Update pypi License (#14)

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Natural Language :: English",
         "Topic :: Software Development :: Libraries :: Python Modules",
