2b5d34a587ba744294c6b4ef0a14a3137546b6c7
raratiru <tantiras@yandex.com>
2020-03-30 01:34:52 +0300

    Added cap_sentence

--- thepysec/__init__.py
+++ thepysec/__init__.py
@@ -8,13 +8,13 @@
 #
 #       Creation Date : Fri 28 Dec 2018 03:36:03 PM EET
 #
-#       Last Modified : Mon 28 Oct 2019 06:44:46 PM EET (18:44)
+#       Last Modified : Mon 30 Mar 2020 01:33:58 AM EEST (01:33)
 #
 # ==============================================================================
 
 from thepysec.john.version import get_version
 
 
-VERSION = (2, 0, 1, "final", 0)
+VERSION = (2, 0, 2, "final", 0)
 
 __version__ = get_version(VERSION)
