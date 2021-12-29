d2f995044d0e660f95cce3c8e31fa22c8698c5a4
raratiru <tantiras@yandex.com>
2021-12-29 18:15:49 +0200

    Blacked

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",        
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Natural Language :: English",
